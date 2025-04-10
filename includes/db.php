01a1834c9de007ebc0f5d1339da9dc4aa34f2e89
Kuda Tesa <89967103+kudatesa@users.noreply.github.com>
2025-04-10 03:09:07 +0200

    Add files via upload

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'zim_resort';
+$user = 'root';
+$pass = '';
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
